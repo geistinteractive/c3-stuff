65ecca344958d7d678c10f2fe7fd0b2bfa3bbf53
Jeremy Brown <jeremy.brown@proofgeist.com>
2021-11-19 14:39:34 -0500

    all commits

--- public/index.js
+++ public/index.js
@@ -1,57 +1,23 @@
 import c3 from "c3";
-import "bootstrap/dist/css/bootstrap.min.css";
-
 import "c3/c3.min.css";
+import { MDCRipple } from "@material/ripple";
+import { chartOneConfig, chartThreeConfig, chartTwoConfig } from "./configs";
+
 window.loadChart = (obj) => {
   const parsed = JSON.parse(obj);
-  console.log(parsed);
   const data = parsed.data;
   const dataTotal = parsed.dataTotal;
-  // return;
-  var chart = c3.generate({
-    bindto: "#chart",
-    data: {
-      json: data,
-      keys: {
-        x: "Date",
-        value: ["Apples", "Peaches"],
-      },
-    },
-    axis: {
-      x: {
-        type: "category",
-      },
-    },
-  });
-  var chartTwo = c3.generate({
-    bindto: "#chartTwo",
-    data: {
-      json: data,
-      keys: {
-        x: "Date",
-        value: ["Bananas", "Grapes", "Strawberries"],
-      },
-    },
-    axis: {
-      x: {
-        type: "category",
-      },
-    },
-  });
-  var chartThree = c3.generate({
-    bindto: "#chartThree",
-    data: {
-      type: "bar",
-      json: dataTotal,
-      keys: {
-        x: "Date",
-        value: ["Total"],
-      },
-    },
-    axis: {
-      x: {
-        type: "category",
-      },
-    },
-  });
+
+  chartOneConfig.data.json = data;
+  chartTwoConfig.data.json = data;
+  chartThreeConfig.data.json = dataTotal;
+
+  chartTwoConfig.data.keys.value = ["Bananas", "Grapes", "Strawberries"];
+  chartOneConfig.bindto = "#chart";
+  chartTwoConfig.bindto = "#chartTwo";
+  chartThreeConfig.bindto = "#chartThree";
+
+  var chart = c3.generate(chartOneConfig);
+  var chartTwo = c3.generate(chartTwoConfig);
+  var chartThree = c3.generate(chartThreeConfig);
 };
